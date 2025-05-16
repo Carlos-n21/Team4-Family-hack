757c980a93d1eb83397c18411c73bef6ffd4094b
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-05-16 23:36:45 +0100

    added profile app and basic setup

--- familytech/urls.py
+++ familytech/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("technicalissues.urls")),
      path("", include("chatbot.urls")),
+     path('profile/', include('userprofile.urls')),
+     path('accounts/', include('django.contrib.auth.urls')),
 ]
