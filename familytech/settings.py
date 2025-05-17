f7bc31dd0a79ded1b2a85b2361f57405f436de01
Cam-4587 Conroy <cameronconroy@live.com>
2025-05-17 14:16:35 +0000

    chore: Add files for deployment and set DEBUG to false

--- familytech/settings.py
+++ familytech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mf%=k3)hcx_%7kki(+co%(rv77tcuday2i@-gn7ys@xst41_ek'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
